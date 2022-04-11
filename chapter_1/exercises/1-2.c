e169a75825f556fce7967e9317612b18523610d6
facu96molina <facu96molina@gmail.com>
2022-04-11 20:44:44 -0300

    Adding solutions

---
+++ chapter_1/exercises/1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+    Experiment to find out what happens when printf's argument string
+    contains \c, where c is some character not listed above.
+*/
+int main()
+{
+    printf("hello, world\n");
+}
