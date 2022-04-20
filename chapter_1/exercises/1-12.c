a518424ed5af44f40c44163ee987a8173a9effe7
facu96molina <facu96molina@gmail.com>
2022-04-19 21:31:26 -0300

    Solutions for 1.5.4

---
+++ chapter_1/exercises/1-12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN  1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+/*
+    Write a program that prints its input one word per line.
+*/
+int main()
+{
+    int c, state;
+
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t'){
+            if (state == IN){
+                state = OUT;
+                printf("\n");
+            }
+        } else {
+            if (state == OUT)
+                state = IN;
+            putchar(c);
+        }
+    }
+}
